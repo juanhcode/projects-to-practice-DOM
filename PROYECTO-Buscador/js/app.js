f85d57c4107a53e2ef9fdb31211a1e8e906dcc2d
Juan Manuel <juanhoyos1347@gmail.com>
2021-08-28 18:22:44 -0500

    se genera el select de los años

--- PROYECTO-Buscador/js/app.js
+++ PROYECTO-Buscador/js/app.js
@@ -1,20 +1,43 @@
 //Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+const max = new Date().getFullYear();
+const min = max - 10;
 
 //Eventos
-document.addEventListener("DOMContentLoaded",() =>{
+document.addEventListener("DOMContentLoaded", () => {
     mostrarAutos();
+    llenarSelectYear();
 });
 
 
 //Funciones
 function mostrarAutos() {
-    autos.forEach(auto =>{
-        const {marca,modelo,year,precio,puertas,color,transmision} = auto;
+    autos.forEach(auto => {
+        const {
+            marca,
+            modelo,
+            year,
+            precio,
+            puertas,
+            color,
+            transmision
+        } = auto;
         const autoHtml = document.createElement('p');
-        autoHtml.textContent=`
+        autoHtml.textContent = `
             ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmision ${transmision} - Precio:${precio} - Color:${color} 
         `;
         resultado.appendChild(autoHtml);
     });
+}
+
+function llenarSelectYear() {
+
+    for (let i = max; i >= min; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
+
 }
