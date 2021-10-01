9c6b51ed4c8627b1b1879149772ea6864ad6947a
Juan Hoyos <juanhoyos1347@gmail.com>
2021-09-30 19:41:56 -0500

    se agrego el spinner y el mensaje de la xcotizacion

--- PROYECTO-Prototypes-Seguro/js/app.js
+++ PROYECTO-Prototypes-Seguro/js/app.js
@@ -11,8 +11,8 @@ Seguro.prototype.cotizarSeguro = function () {
     2 =  Asiatico 1.05
     3 =  Europeo 1.35
     */
-   let cantidad;
-   let base = 2000;
+    let cantidad;
+    let base = 2000;
     switch (this.marca) {
         case '1':
             cantidad = base * 1.15;
@@ -29,16 +29,16 @@ Seguro.prototype.cotizarSeguro = function () {
     //leer año
     const diferencia = new Date().getFullYear() - this.year;
     //Cada año que la diferencia es mayor , el costo va reducirse un 3%
-    cantidad -= ((diferencia * 3) * cantidad)  / 100;
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
     console.log(cantidad);
 
-    if(this.tipo === 'basico'){
+    if (this.tipo === 'basico') {
         cantidad *= 1.30;
-    }else{
+    } else {
         cantidad *= 1.50;
     }
 
-    return cantidad;
+    return cantidad
 }
 
 function UI() {}
@@ -73,6 +73,53 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
     }, 3000)
 }
 
+UI.prototype.mostrarResultado = (seguro, total) => {
+
+    //crear el resultados
+    const div = document.createElement("div");
+    div.classList.add('mt-10');
+
+    const {
+        marca,
+        year,
+        tipo
+    } = seguro;
+    console.log(marca)
+    let texto;
+    switch (marca) {
+        case '1':
+            texto = 'Americano'
+            break;
+        case '2':
+            texto = 'Asiatico'
+            break;
+        case '3':
+            texto = 'Europeo'
+            break;
+        default:
+            break;
+    }
+
+    div.innerHTML = `
+        <p class="header">Tu Resumen</p>
+        <p class="font-bold">Marca:${texto} </p>
+        <p class="font-bold">Año:${year} </p>
+        <p class="font-bold">Tipo:${tipo} </p>
+        <p class="font-bold">Total:$${total} </p>
+
+    `;
+    const resultado = document.querySelector('#resultado');
+    //mostrar el spinner
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display = 'none';
+        resultado.appendChild(div);
+    }, 3000)
+
+}
+
 //instanciar UI
 const ui = new UI();
 document.addEventListener("DOMContentLoaded", () => {
@@ -80,10 +127,12 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 evenListener();
+
 function evenListener() {
     const formulario = document.querySelector('#cotizar-seguro');
     formulario.addEventListener('submit', cotizarSeguro)
 }
+
 function cotizarSeguro(e) {
     e.preventDefault();
     //leer la marca seleccionado
@@ -97,9 +146,19 @@ function cotizarSeguro(e) {
         return;
     }
     ui.mostrarMensaje('Cotizando......', 'exito');
+    //pculta cotizaciones previas
+    const resultados = document.querySelector('#resultado div');
+    if (resultados != null) {
+        resultados.remove();
+    }
+
+
     //Instanciar el seguro
     const seguro = new Seguro(marca, year, tipo);
     const total = seguro.cotizarSeguro();
-    
+    //mandar valores de seguro y total
+    ui.mostrarResultado(seguro, total);
 
+    //cada año que la diferencia es mayor , el costo se reduce en 3%
+    const diferencia = new Date().getFullYear() - this.year;
 }
